2958db3f901b14cde337b5f1a2e7a919cc3fed68
emmanbobo <olawalealawode@gmail.com>
2025-04-14 14:10:24 +0100

    done error fixed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,4 @@
-import Navbar from "../components/Navbar";
+
 
 export default function RootLayout({
   children,
@@ -7,7 +7,6 @@ export default function RootLayout({
 }) {
   return (
     <main className="font-sans">
-        <Navbar />
         {children}
     </main>
   );
