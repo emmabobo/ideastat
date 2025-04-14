3189374c74a0a6e56f7fe75bf3e5ea6f8ebaee17
emmanbobo <olawalealawode@gmail.com>
2025-04-14 10:48:49 +0100

    trying

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { auth, signIn, signOut } from "@/auth";
+import { auth } from "@/auth";
 
 const Navbar = async () => {
   const session = await auth();
