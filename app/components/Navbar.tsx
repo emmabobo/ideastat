935476b13f30cc4749646fc32d6a8665016a3185
emmanbobo <olawalealawode@gmail.com>
2025-04-15 15:37:04 +0100

    Done

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,23 +1,24 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { auth, signOut, signIn } from "@/auth"; 
+import { redirect } from 'next/dist/server/api-utils';
 
 const Navbar = async () => {
 
-  const session = await auth();
+  
+  const session = await auth;
 
   return (
-    < >
     <header className='px-5 py-3 bg-white font-work-sans'>
       <nav className='flex justify-between items-center'>
 
         <Link href="/" className="flex items-center gap-2">
-          <Image src="/logo.svg" width={40} height={40} alt="Logo" className="rounded-full" />
-          <span className="font-bold text-xl">IdeaStat</span>
+          <Image src="/logo.svg" width={150} height={150} alt="Logo" />
+          {/* <span className="font-bold text-xl">IdeaStat</span> */}
         </Link>
 
         <div className='flex items-center gap-5 text-blaclk'>
-          {session && session?.user ? (
+          {session?.user ? (
             <>
               <Link href="/dashboard" className=" hover:text-gray-700">
                 <span>Dashboard</span>
@@ -36,18 +37,18 @@ const Navbar = async () => {
                   <button type='submit'>Logout</button>
               </form>
 
-              <Link href={`/user/${session?.id}`}>
-                <span>{session?.user?.name}</span>
+              <Link href={`/user/${session.user.id}`}>
+                <span>{session.user.name}</span>
               </Link>
               
-              </>
+            </>
           ):(
               
               <form action={async () =>{
                 'use server';
-                await signIn('github');
+                await signIn ('github', { redirect: '/' });
                 } }>
-                <button>
+                <button type='submit'>
                   Login  
                 </button>
               </form>
@@ -61,8 +62,7 @@ const Navbar = async () => {
       </nav>
     </header>
       
-    </>
-  )
-}
+  );
+};
 
 export default Navbar;
