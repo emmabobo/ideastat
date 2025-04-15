ff1ae4df6f69b11c04d739bd95a21e00d18df0a3
emmanbobo <olawalealawode@gmail.com>
2025-04-15 09:50:40 +0100

    auth

---
+++ auth.ts
@@ -0,0 +1,12 @@
+// auth.ts
+
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+
+export const { auth, signOut, signIn, handlers } = NextAuth({
+  providers: [GitHub({
+    clientId: 'process.env.GITHUB_CLIENT_ID' ,
+    clientSecret: 'process.env.GITHUB_CLIENT_SECRET' ,
+  }),],
+  secret: process.env.NEXTAUTH_SECRET,
+})
